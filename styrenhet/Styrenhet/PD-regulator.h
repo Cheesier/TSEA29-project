68f954cfa3492a7891a65a591b98554dcf2d1259
jonny928 <jonny928@student.liu.se>
2014-11-17 12:43:10 +0100

    Added PD-regulator

---
+++ styrenhet/Styrenhet/PD-regulator.h
@@ -0,0 +1,18 @@
+/*
+ * PD_regulator.h
+ *
+ * Created: 2014-11-17 12:10:19
+ *  Author: Jonathan
+ */ 
+
+
+#ifndef PD-REGULATOR_H_
+#define PD-REGULATOR_H_
+
+void activate();
+void deactivate();
+void updateSensorData(uint8_t left, uint8_t right);
+
+
+
+#endif /* PD-REGULATOR_H_ */
