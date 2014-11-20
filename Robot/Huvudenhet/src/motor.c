84924894304602eb37cf05f412c22e99fbf840d9
Oscar Thunberg <ostenip@gmail.com>
2014-11-20 15:52:58 +0100

    Added more motor control commands

--- Robot/Huvudenhet/src/motor.c
+++ Robot/Huvudenhet/src/motor.c
@@ -12,14 +12,36 @@ void init_motor(void) {
 	// whatever there is to do here
 }
 
-void motor_change_dir(char dir) {
-	//send_message_to(ADDR_STYRENHET, 0x04, 1, )
-}
-
 void motor_claw_close(void) {
 	send_message_to(ADDR_STYRENHET, 0x0B, 0, NO_DATA);
 }
 
 void motor_claw_open(void) {
 	send_message_to(ADDR_STYRENHET, 0x0C, 0, NO_DATA);
+}
+
+void motor_set_direction(char dir) {
+	char data[] = {dir};
+	send_message_to(ADDR_STYRENHET, 0x04, 1, (char*)&data);
+}
+
+void motor_set_speed(char speed) {
+	char data[] = {speed};
+	send_message_to(ADDR_STYRENHET, 0x0E, 1, (char*)&data);
+}
+
+void motor_stop(void) {
+	send_message_to(ADDR_STYRENHET, 0x0D, 0, NO_DATA);
+}
+
+void motor_go_forward(void) {
+	send_message_to(ADDR_STYRENHET, 0x07, 0, NO_DATA);
+}
+
+void motor_go_backward(void) {
+	send_message_to(ADDR_STYRENHET, 0x08, 0, NO_DATA);
+}
+
+void motor_rotate_right(void) {
+	send_message_to(ADDR_STYRENHET, 0x0A, 0, NO_DATA);
 }
