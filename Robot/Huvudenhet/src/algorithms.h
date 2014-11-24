514319df3677376694364473a5e199cfc720167e
jonny928 <jonny928@student.liu.se>
2014-11-24 16:46:27 +0100

    First prototype of detection of different cross-sections

---
+++ Robot/Huvudenhet/src/algorithms.h
@@ -0,0 +1,19 @@
+/*
+ * algorithms.h
+ *
+ * Created: 2014-11-24 12:57:04
+ *  Author: Jonathan
+ */ 
+
+
+#ifndef ALGORITHMS_H_
+#define ALGORITHMS_H_
+
+#include "message_handler.h"
+#include <avr/io.h>
+
+void interpretSensorData(char * sensorData);
+
+
+
+#endif /* ALGORITHMS_H_ */
