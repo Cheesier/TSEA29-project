dff3a4889a4fc697f18d562f0d5c9ab030925168
Erik Sandberg <e.sandberg92@gmail.com>
2014-11-25 14:22:44 +0100

    merging

--- sensorenhet/sensorenhet/gyro.c
+++ sensorenhet/sensorenhet/gyro.c
@@ -8,15 +8,18 @@
 #define F_CPU 8000000UL
 
 #include <stdint.h>
+#include <stdlib.h>
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
+/*
 
 #include "ADC.h"
-#include "gyro.h"
+#include "gyro.h"*/
+#include "sensorenhet.h"
 
 
-#define START_TIMER TCCR1B |= (1 << CS10) | (1 << CS12);
+#define START_TIMER TCCR1B |= (1 << CS10) | (1 << CS12);	// Set the prescaler to 1024
 #define STOP_TIMER TCCR1B &= ~((1 << CS10) | (1 << CS12));
 
 static int gyro_null_value = 0;
@@ -63,8 +66,9 @@ int updateGyroData() {
 	// returned value is 100 times bigger than it should be
 
 	// Leave function when 10 ms have passed since we started
-	while (!(TIFR & (1 << OCF1B))) {};
+	while (!(TIFR & (1 << OCF1B)));
 	STOP_TIMER;
+	TIFR |= (1 << OCF1A);	// Reset the timer flag
 
 	return gyro_value;
 }
@@ -82,10 +86,10 @@ void resetDegreesRotated(){
 // and then read the value of the gyro to get a standard value
 void initGyro() {
 	_delay_ms(1000);
-	gyro_null_value = gyroADC();						// Should give us a value of approximately 512
+	gyro_null_value = gyroADC();	// Should give us a value of approximately 512
 
 
 	// Initialize timer 1
-	TCCR1B |= (1 << WGM12) | (1 << CS12) | (1 << CS10);	// Set mode to CTC and prescaler to 1024
-	OCR1B = 77;											// This value with the 1024 prescaler result in the timer running for 10 ms
+	TCCR1B |= (1 << WGM12);			// Set mode to CTC
+	OCR1B = 77;						// This value with the 1024 prescaler result in the timer running for 10 ms
 }
