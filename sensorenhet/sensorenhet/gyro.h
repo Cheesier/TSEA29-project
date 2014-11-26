7cd79e4e01dfd3dcee56d0608a9815c4d9ebd571
Erik Sandberg <e.sandberg92@gmail.com>
2014-11-26 11:42:58 +0100

    merging

--- sensorenhet/sensorenhet/gyro.h
+++ sensorenhet/sensorenhet/gyro.h
@@ -12,16 +12,11 @@
 extern uint16_t gyro_data_done;
 
 void rotateDegrees(uint16_t degrees);
-//<<<<<<< HEAD
 void updateGyroData();
 uint8_t returnGyroData(uint8_t gyro_data);
-uint8_t returnDegreesRotated();
-/*
-=======
-int updateGyroData();
-int returnGyroData(int gyro_data);
 int returnDegreesRotated();
->>>>>>> origin/master*/
+/*
+int returnGyroData(int gyro_data); */
 void resetDegreesRotated();
 
 void initGyro();
