6b6347af9e2ef4b61f0e482fb6875c4f23c57cb8
Rasmus Thuresson <rasmusthuresson@msn.com>
2014-12-01 12:46:28 +0100

    Missed a few lines..

--- styrenhet/Styrenhet/Styrenhet.c
+++ styrenhet/Styrenhet/Styrenhet.c
@@ -28,6 +28,7 @@
 #define REVERSE 0
 
 char direction = FORWARD;
+#define MAXSPEED 255
 uint8_t rotateSpeed = 128;
 uint8_t speed = 128;
 
@@ -118,16 +119,16 @@ void rotateRight() {
 // The robot makes a soft turn
 // Turn direction is decided by the input parameters
 void PDTurning(uint16_t left_speed, uint16_t right_speed) {
-	left_speed = (left_speed * speed)/255;
-	right_speed = (right_speed * speed)/255;
+	left_speed = (left_speed * speed)/MAXSPEED;
+	right_speed = (right_speed * speed)/MAXSPEED;
 	if(direction == REVERSE) {
 		PORTA &= ~(1<<WHEEL_DIRECTION_L);
 		PORTA &= ~(1<<WHEEL_DIRECTION_R);
-		setSpeeds(right_speed, left_speed);
-		return;		
-	}	
-	PORTA |= (1<<WHEEL_DIRECTION_L);
-	PORTA |= (1<<WHEEL_DIRECTION_R);
+	}
+	else {	
+		PORTA |= (1<<WHEEL_DIRECTION_L);
+		PORTA |= (1<<WHEEL_DIRECTION_R);
+	}
 	setSpeeds(left_speed, right_speed);				// The PWM implementation can handle separate speeds for both sides
 	return;
 }
